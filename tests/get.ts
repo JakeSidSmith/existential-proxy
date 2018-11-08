c14e71f2dce7d471522dc23fb6de16139fc682a9
Jake 'Sid' Smith <jake@dabapps.com>
2018-11-08 17:47:16 +0000

    Rename test variables

--- tests/get.ts
+++ tests/get.ts
@@ -21,37 +21,38 @@ describe('get', () => {
 
   it('should return the requested value when it exists', () => {
     const root = get(obj1, proxy => proxy);
-    const foo1 = get(obj1, proxy => proxy.foo);
-    const bar1 = get(obj1, proxy => proxy.foo.bar);
-    const baz1 = get(obj1, proxy => proxy.foo.bar.baz);
+    const foo = get(obj1, proxy => proxy.foo);
+    const bar = get(obj1, proxy => proxy.foo.bar);
+    const baz = get(obj1, proxy => proxy.foo.bar.baz);
 
     expect(root).toEqual({ foo: { bar: { baz: 'baz' } } });
-    expect(foo1).toEqual({ bar: { baz: 'baz' } });
-    expect(bar1).toEqual({ baz: 'baz' });
-    expect(baz1).toBe('baz');
+    expect(foo).toEqual({ bar: { baz: 'baz' } });
+    expect(bar).toEqual({ baz: 'baz' });
+    expect(baz).toBe('baz');
   });
 
   it('should return undefined when the value does not exist', () => {
     const root = get(obj2, proxy => proxy);
-    const foo2 = get(obj2, proxy => proxy.foo);
-    const bar2 = get(obj2, proxy => proxy.foo.bar);
-    const baz2 = get(obj2, proxy => proxy.foo.bar.baz);
+    const foo = get(obj2, proxy => proxy.foo);
+    const bar = get(obj2, proxy => proxy.foo.bar);
+    const baz = get(obj2, proxy => proxy.foo.bar.baz);
 
     expect(root).toEqual({});
-    expect(foo2).toBe(undefined);
-    expect(bar2).toBe(undefined);
-    expect(baz2).toBe(undefined);
+    expect(foo).toBe(undefined);
+    expect(bar).toBe(undefined);
+    expect(baz).toBe(undefined);
   });
 
   it('should return the default value when the value does not exist', () => {
     const root = get(obj2, proxy => proxy, {foo: null});
-    const foo2 = get(obj2, proxy => proxy.foo, {bar: null});
-    const bar2 = get(obj2, proxy => proxy.foo.bar, {baz: 'def'});
-    const baz2 = get(obj2, proxy => proxy.foo.bar.baz, 'def');
+    const foo = get(obj2, proxy => proxy.foo, {bar: null});
+    const bar = get(obj2, proxy => proxy.foo.bar, {baz: 'def'});
+    const baz = get(obj2, proxy => proxy.foo.bar.baz, 'def');
 
     expect(root).toEqual({});
-    expect(foo2).toEqual({bar: null});
-    expect(bar2).toEqual({baz: 'def'});
-    expect(baz2).toBe('def');
+    expect(foo).toEqual({bar: null});
+    expect(bar).toEqual({baz: 'def'});
+    expect(baz).toBe('def');
+  });
   });
 });
