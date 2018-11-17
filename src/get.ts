252ef6a5e24d06fad3cd5c40dfe885635abca92d
Jake 'Sid' Smith <jake@dabapps.com>
2018-11-17 00:39:08 +0000

    Prettier

--- src/get.ts
+++ src/get.ts
@@ -8,7 +8,11 @@ export interface WithProxyArray<T, U> extends ReadonlyArray<WithProxy<T | U>> {}
 
 export type WithProxyObject<T, U> = { [P in keyof T]-?: WithProxy<T[P] | U> };
 
-export type WithProxy<T, U = T extends undefined | null ? undefined : never, S = Exclude<T, undefined | null>> = S extends object
+export type WithProxy<
+  T,
+  U = T extends undefined | null ? undefined : never,
+  S = Exclude<T, undefined | null>
+> = S extends object
   ? WithProxyObject<T, U> & AccessProxy<T>
   : S extends ReadonlyArray<infer V>
   ? WithProxyArray<V, U> & AccessProxy<T>
