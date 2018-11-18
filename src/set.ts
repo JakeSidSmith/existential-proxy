14fd98ee4f9d06d27fba02db991530feac2d9d84
Jake 'Sid' Smith <jake@dabapps.com>
2018-11-18 13:46:35 +0000

    Create set function

---
+++ src/set.ts
@@ -0,0 +1,49 @@
+import { WithProxy } from './types';
+
+export function setIn<T extends object>(
+  input: T,
+  keys: ReadonlyArray<string | number>,
+  newValue: any
+) {
+  if (!keys.length) {
+    return newValue;
+  }
+
+  const copy = Array.isArray(input) ? [...input] : { ...(input as any) };
+  const [firstKey, ...restKeys] = keys;
+
+  if (copy[firstKey] === null || typeof copy[firstKey] === 'undefined') {
+    copy[firstKey] = typeof firstKey === 'number' ? [] : {};
+  }
+
+  copy[firstKey] = setIn(copy[firstKey], restKeys, newValue);
+
+  return copy;
+}
+
+export function set<T extends object, R>(
+  input: T,
+  callback: (input: WithProxy<T>) => WithProxy<R>,
+  newValue: R
+): T {
+  const keys: Array<string | number> = [];
+
+  const handlers = {
+    get<S extends object>(value: S, key: keyof S): object {
+      const currentValue: any = value[key];
+      keys.push(key as string | number);
+
+      if (currentValue && typeof currentValue === 'object') {
+        return new Proxy(currentValue, handlers);
+      }
+
+      return new Proxy({}, handlers);
+    },
+  };
+
+  const proxy = new Proxy(input, handlers) as WithProxy<T>;
+
+  callback(proxy);
+
+  return setIn(input, keys, newValue);
+}
