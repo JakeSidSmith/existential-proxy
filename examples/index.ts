d19069263be627cd57d34b70dbd3e0af25bb1c3c
Jake 'Sid' Smith <jake@dabapps.com>
2018-11-18 15:08:34 +0000

    Add additional example

--- examples/index.ts
+++ examples/index.ts
@@ -17,3 +17,16 @@ const abc: ABC = {
 const b = ep.get(abc, proxy => proxy.a.b, { c: 'c' });
 
 console.log(b);
+
+interface NumKey {
+  a?: {
+    0: string;
+  };
+}
+
+const numKey: NumKey = {};
+
+// Will create an array when trying to access the `0` key
+const numKeyResult = ep.set(numKey, (proxy) => proxy.a[0], 'hello'); // { a: ['hello'] }:
+
+console.log(numKeyResult);
