38a9822c6b0b58cfaba1ce44a7ae9c9c48fd33c7
Jake 'Sid' Smith <jake@dabapps.com>
2018-11-18 18:52:11 +0000

    Additional MATCHES_INT tests

--- tests/constants.ts
+++ tests/constants.ts
@@ -9,6 +9,7 @@ describe('constants', () => {
 
   describe('MATCHES_INT', () => {
     it('should test for strings that represent valid integers', () => {
+      expect(CONSTANTS.MATCHES_INT.test('')).toBe(false);
       expect(CONSTANTS.MATCHES_INT.test('0.1')).toBe(false);
       expect(CONSTANTS.MATCHES_INT.test('-0.1')).toBe(false);
       expect(CONSTANTS.MATCHES_INT.test('1a')).toBe(false);
@@ -18,6 +19,8 @@ describe('constants', () => {
 
       expect(CONSTANTS.MATCHES_INT.test('1')).toBe(true);
       expect(CONSTANTS.MATCHES_INT.test('-1')).toBe(true);
+      expect(CONSTANTS.MATCHES_INT.test('123')).toBe(true);
+      expect(CONSTANTS.MATCHES_INT.test('-123')).toBe(true);
     });
   });
 });
